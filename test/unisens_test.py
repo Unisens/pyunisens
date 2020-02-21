71b3cb022f57306b5d6f99a022d2a13cc03ce9bd
skjerns <14980558+skjerns@users.noreply.github.com>
2020-02-21 18:04:31 +0100

    fix ut

--- test/unisens_test.py
+++ test/unisens_test.py
@@ -5,6 +5,8 @@ Created on Sat Jan  4 16:37:07 2020
 @author: skjerns
 """
 import os
+import sys
+sys.path.append('..')
 try:
     from unisens import CustomEntry, ValuesEntry, EventEntry, SignalEntry
     from unisens import MiscEntry, CustomAttributes, Unisens, FileEntry
