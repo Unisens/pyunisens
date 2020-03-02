08b2b291e89ded9029812c7da045185539315207
skjerns <14980558+skjerns@users.noreply.github.com>
2020-03-02 18:51:41 +0100

    remove author

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='pyunisens',
       description='A python implementation of the Unisens standard',
       url='http://github.com/skjerns/pyUnisens',
       author='skjerns',
-      author_email='nomail',
       license='GNU 2.0',
       packages=['unisens'],
       install_requires=[
