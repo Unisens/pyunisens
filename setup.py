c3ed0f1f32701b7844de2739898c62c6af380ef1
skjerns <14980558+skjerns@users.noreply.github.com>
2021-03-08 14:51:18 +0100

    adapted setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pyunisens',
       python_requires='>=3.6',
-      version='1.02',
+      version='1.03',
       description='A python implementation of the Unisens standard',
-      url='http://github.com/skjerns/pyUnisens',
+      url='http://github.com/Unisens/pyUnisens',
       author='skjerns',
       license='GNU 2.0',
       packages=['unisens'],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       install_requires=[
          'numpy',
          'datetime',
          'pandas'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
       zip_safe=False)
